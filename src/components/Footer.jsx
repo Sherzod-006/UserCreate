d58b80771f92e756630dc6f9fef70677496e0f9f
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-08-27 22:02:11 +0500

    footer section completed.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-gray-800 text-white p-6 mt-auto'>
-      <h1 className="text-3xl font-bold underline">
-        Footer Component
-      </h1>
+    <footer className='bg-gray-800 text-white p-5 flex items-center justify-between px-15 mt-auto'>
+      <ul>
+        <li>Address: Chust City, Uchqun St</li>
+        <li>Email: sherzodsobirovv1@gmail.com</li>
+        <li>Phone: (+998) 90-222-79-06</li>
+      </ul>
+      <p className='text-lg'>&copy; 2024 UserCR. All rights reserved.</p>
     </footer>
   )
 }
